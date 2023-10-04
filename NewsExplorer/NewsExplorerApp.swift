50da017abc29c6517b34f99f26ccb1db71721eb1
MityA32 <hetmandmitriy@gmail.com>
2023-10-04 22:25:28 +0300

    Initial Commit

---
+++ NewsExplorer/NewsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerApp.swift
+//  NewsExplorer
+//
+//  Created by Dmytro Hetman on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
